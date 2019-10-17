e320bdeb1b70cf0047c60265a1ccfd179d6ba015
Waleed Rafi <waleed.rafi626@gmail.com>
2019-10-17 17:45:11 +0500

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UploadImageComponent } from './upload-image/upload-image.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UploadImageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
